0017b7b72e35ac7d5f83df491f8033d60e9ae3e3
Grace Braden <gracebraden97@gmail.com>
2018-03-13 21:59:18 -0500

    Initial commit

---
+++ src/com/company/BTree.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class BTree {
+    //public BTree root;
+    public BTree left;
+    public BTree right;
+    public int value;
+
+    public BTree (int value) {
+        this.value = value;
+        left = null;
+        right = null;
+    }
+
+    
+
+
+
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
