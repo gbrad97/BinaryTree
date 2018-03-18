bd39990d71b93c8e83be952d1e7939ff2f834ca5
Grace Braden <gracebraden97@gmail.com>
2018-03-18 02:43:40 -0500

    Add insert method Add tree traversal methods

--- src/com/company/BTree.java
+++ src/com/company/BTree.java
@@ -1,24 +1,104 @@
 package com.company;
 
 public class BTree {
-    //public BTree root;
-    public BTree left;
-    public BTree right;
+    public BTNode root;
     public int value;
 
     public BTree (int value) {
         this.value = value;
-        left = null;
-        right = null;
+        this.root = new BTNode(value);
     }
 
-    
-
-
-
-
-    @Override
-    public String toString() {
-        return super.toString();
+    // calls addBST to start at the root of the tree
+    public BTree insertBST(int value) {
+        BTree tree = this;
+        tree.root = addBST(tree.root, value);
+        return tree;
     }
+
+    private BTNode addBST(BTNode currentNode, int value) {
+
+        if (currentNode == null) {
+            return new BTNode(value);
+        }
+        else {
+            if (value <= currentNode.value) {
+                currentNode.left = addBST(currentNode.left, value);
+            }
+            else {
+                currentNode.right = addBST(currentNode.right, value);
+            }
+        }
+        return currentNode;
+    }
+
+
+    public void outputPreOrder() {
+        preOrderTraversal(root);
+    }
+
+    public void outputInOrder() {
+        inOrderTraversal(root);
+    }
+
+    public void outputPostOrder() {
+        postOrderTraversal(root);
+    }
+
+
+
+    private void preOrderTraversal(BTNode currentNode) {
+        if (currentNode == null) {
+            System.out.print("");
+        }
+        else {
+            System.out.print(currentNode.value + " ");
+            preOrderTraversal(currentNode.left);
+            preOrderTraversal(currentNode.right);
+        }
+    }
+
+    private void inOrderTraversal(BTNode currentNode) {
+        if (currentNode == null) {
+            System.out.print("");
+        }
+        else {
+            inOrderTraversal(currentNode.left);
+            System.out.print(currentNode.value + " ");
+            inOrderTraversal(currentNode.right);
+        }
+    }
+
+    private void postOrderTraversal(BTNode currentNode) {
+        if (currentNode == null) {
+            System.out.print("");
+        }
+        else {
+            postOrderTraversal(currentNode.left);
+            postOrderTraversal(currentNode.right);
+            System.out.print(currentNode.value + " ");
+        }
+    }
+
+
+    public class BTNode {
+        int value;
+        BTNode left;
+        BTNode right;
+
+        public BTNode(int value) {
+            this.value = value;
+            left = null;
+            right = null;
+        }
+
+    }
+
+
+
+
+
+
+
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        BTree btree = new BTree(5);
+        btree.insertBST(3);
+        btree.insertBST(1);
+        btree.insertBST(4);
+        btree.insertBST(10);
+        btree.insertBST(15);
+        btree.insertBST(7);
+        btree.insertBST(11);
+       
+        System.out.print("Tree (PreOrder) (5 3 1 4 10 7 15 11): ");
+        btree.outputPreOrder();
+
+        System.out.print("\nTree (InOrder) (1 3 4 5 7 10 11 15): ");
+        btree.outputInOrder();
+
+        System.out.print("\nTree (PostOrder) (1 4 3 7 11 15 10 5): ");
+        btree.outputPostOrder();
+
     }
 }
